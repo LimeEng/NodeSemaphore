2d7a3b5bd0f890a730ca572dbb0d54227314651e
Emil Englesson <englesson.emil@gmail.com>
2018-10-16 17:34:34 +0200

    Implemented the rest of the tests
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- test/utility.test.js
+++ test/utility.test.js
@@ -34,23 +34,55 @@ describe('Utility', function () {
   describe('isInteger(value)', function () {
     const isInteger = utility.isInteger
     it('should correctly detect integers', function () {
-      // TODO
-      this.skip()
+      for (let i = -100; i <= 100; i++) {
+        assert.ok(isInteger(i))
+      }
+      assert.ok(isInteger(Number.MAX_SAFE_INTEGER))
+      assert.ok(isInteger(Number.MIN_SAFE_INTEGER))
     })
     it('should correctly return false', function () {
-      // TODO
-      this.skip()
+      assert.ok(!isInteger(0.9999999999999))
+      assert.ok(!isInteger(0.0000000000001))
+      assert.ok(!isInteger(Number.NaN))
+      assert.ok(!isInteger(Number.POSITIVE_INFINITY))
+      assert.ok(!isInteger(Number.NEGATIVE_INFINITY))
+      assert.ok(!isInteger(Number.MAX_VALUE))
+      assert.ok(!isInteger(Number.MIN_VALUE))
+      assert.ok(!isInteger([]))
+      assert.ok(!isInteger([1]))
+      assert.ok(!isInteger([1, 2, 3]))
+      assert.ok(!isInteger({}))
+      assert.ok(!isInteger({ 1: 2 }))
+      assert.ok(!isInteger(() => { 1 + 1 }))
     })
   })
   describe('isStrictPositiveInteger(value)', function () {
     const isStrictPositiveInteger = utility.isStrictPositiveInteger
     it('should correctly detect strictly positive integers (> 0)', function () {
-      // TODO
-      this.skip()
+      for (let i = 1; i <= 100; i++) {
+        assert.ok(isStrictPositiveInteger(i))
+      }
+      assert.ok(isStrictPositiveInteger(Number.MAX_SAFE_INTEGER))
+
     })
     it('should correctly return false', function () {
-      // TODO
-      this.skip()
+      for (let i = -100; i <= 0; i++) {
+        assert.ok(!isStrictPositiveInteger(i))
+      }
+      assert.ok(!isStrictPositiveInteger(0.9999999999999))
+      assert.ok(!isStrictPositiveInteger(0.0000000000001))
+      assert.ok(!isStrictPositiveInteger(Number.NaN))
+      assert.ok(!isStrictPositiveInteger(Number.POSITIVE_INFINITY))
+      assert.ok(!isStrictPositiveInteger(Number.NEGATIVE_INFINITY))
+      assert.ok(!isStrictPositiveInteger(Number.MAX_VALUE))
+      assert.ok(!isStrictPositiveInteger(Number.MIN_VALUE))
+      assert.ok(!isStrictPositiveInteger([]))
+      assert.ok(!isStrictPositiveInteger([1]))
+      assert.ok(!isStrictPositiveInteger([1, 2, 3]))
+      assert.ok(!isStrictPositiveInteger({}))
+      assert.ok(!isStrictPositiveInteger({ 1: 2 }))
+      assert.ok(!isStrictPositiveInteger(() => { 1 + 1 }))
+      assert.ok(!isStrictPositiveInteger(Number.MIN_SAFE_INTEGER))
     })
   })
 })
