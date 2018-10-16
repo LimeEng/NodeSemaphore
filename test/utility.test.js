9b1783a5a036b23aa32ec9bdabc36cb542a51764
Emil Englesson <englesson.emil@gmail.com>
2018-10-16 11:57:43 +0200

    Set up a test skeleton to be implemented
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

---
+++ test/utility.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert').strict
+const utility = require('../lib/utility.js')
+
+describe('Utility', function () {
+  describe('isFunction(value)', function () {
+    it('should correctly detect functions', function () {
+      // TODO
+      this.skip()
+    })
+  })
+  describe('isInteger(value)', function () {
+    it('should correctly detect integers', function () {
+      // TODO
+      this.skip()
+    })
+  })
+  describe('isStrictPositiveInteger(value)', function () {
+    it('should correctly detect strictly positive integers (> 0)', function () {
+      // TODO
+      this.skip()
+    })
+  })
+})
