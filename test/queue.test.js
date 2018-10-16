0d27436bc9c1b620f870710993dfa387ce8b4e94
Emil Englesson <englesson.emil@gmail.com>
2018-10-16 19:13:37 +0200

    Created a test skeleton to prepare testing
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

---
+++ test/queue.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const assert = require('assert').strict
+const utility = require('../lib/queue.js')
+
+describe('Queue', function () {
+  describe('.offer(value)', function () {
+  
+  })
+  describe('.poll()', function () {
+  
+  })
+  describe('.peek()', function () {
+  
+  })
+  describe('.clear()', function () {
+  
+  })
+})
