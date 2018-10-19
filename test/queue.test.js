ff571f1f3cf773411bad4cbcd425b8a2d610c7a4
Emil Englesson <englesson.emil@gmail.com>
2018-10-19 22:33:01 +0200

    Implemented new tests and moved away from the deprecated deepEqual. Also switched around arguments to deepStrictEqual so that the parameters 'actual' and 'expected' line up as they should
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,142 +1,155 @@
 'use strict'
 
 const assert = require('assert').strict
+const testing = require('./test-utility.js')
 const Queue = require('../lib/queue.js')
 
 describe('Queue', function () {
   describe('constructor', function () {
     it('should be empty when instantiated', function () {
       const queue = new Queue()
-      assert.deepEqual(0, queue.size)
-      assert.deepEqual(true, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 0)
+      assert.deepStrictEqual(queue.isEmpty, true)
     })
   })
+
   describe('.offer(...values)', function () {
     it('should update size and isEmpty', function () {
       const queue = new Queue()
       queue.offer(1, 2, 3, 4, 5)
-      assert.deepEqual(5, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 5)
+      assert.deepStrictEqual(queue.isEmpty, false)
     })
+
     it('should not break down arrays', function () {
       const queue = new Queue()
       const data = [1, 2, 3]
       queue.offer(data, 4, 5, 6)
-      assert.deepEqual(4, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
-      assert.deepEqual(data, queue.peek())
+      assert.deepStrictEqual(queue.size, 4)
+      assert.deepStrictEqual(queue.isEmpty, false)
+      assert.deepStrictEqual(queue.peek(), data)
     })
+
     it('should throw an error if offered null', function () {
       const queue = new Queue()
-      let errorThrown = false
-      try {
-        queue.offer(1, 2, 3, null, 4, 5, 6)
-      } catch (err) {
-        errorThrown = true
-      }
-      assert.deepEqual(true, errorThrown)
+      const errorThrown = testing.shouldThrow(() => queue.offer(1, 2, 3, null, 4, 5, 6))
+      assert.deepStrictEqual(errorThrown, true)
     })
+
+    it('should throw an error if offered undefined', function () {
+      const queue = new Queue()
+      const errorThrown = testing.shouldThrow(() => queue.offer(1, 2, 3, undefined, 4, 5, 6))
+      assert.deepStrictEqual(errorThrown, true)
+    })
+
     it('should add items in the correct order', function () {
       const queue = new Queue()
-      const data = array(10)
+      const data = testing.zeroTo(10)
       queue.offer(...data)
-      assert.deepEqual(10, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 10)
+      assert.deepStrictEqual(queue.isEmpty, false)
 
       const polled = data.map(item => queue.poll())
 
-      assert.deepEqual(data, polled)
+      assert.deepStrictEqual(polled, data)
     })
   })
+
   describe('.offer(value)', function () {
     it('should update size and isEmpty', function () {
       const queue = new Queue()
       for (let i = 1; i < 10; i++) {
         queue.offer(i)
-        assert.deepEqual(i, queue.size)
-        assert.deepEqual(false, queue.isEmpty)
+        assert.deepStrictEqual(queue.size, i)
+        assert.deepStrictEqual(queue.isEmpty, false)
       }
     })
+
     it('should not break down arrays', function () {
       const queue = new Queue()
       const data = [1, 2, 3, 4, 5]
       queue.offer(data)
-      assert.deepEqual(1, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
-      assert.deepEqual(data, queue.peek())
+      assert.deepStrictEqual(queue.size, 1)
+      assert.deepStrictEqual(queue.isEmpty, false)
+      assert.deepStrictEqual(queue.peek(), data)
     })
+
     it('should throw an error if offered null', function () {
       const queue = new Queue()
-      let errorThrown = false
-      try {
-        queue.offer(null)
-      } catch (err) {
-        errorThrown = true
-      }
-      assert.deepEqual(true, errorThrown)
+      const errorThrown = testing.shouldThrow(() => queue.offer(null))
+      assert.deepStrictEqual(errorThrown, true)
+    })
+
+    it('should throw an error if offered undefined', function () {
+      const queue = new Queue()
+      const errorThrown = testing.shouldThrow(() => queue.offer(undefined))
+      assert.deepStrictEqual(errorThrown, true)
     })
   })
+
   describe('.poll()', function () {
-    it('should return null if called on an empty queue', function () {
+    it('should return undefined if called on an empty queue', function () {
       const queue = new Queue()
-      assert.deepEqual(null, queue.poll())
+      assert.deepStrictEqual(queue.poll(), undefined)
     })
+
     it('should return the right element and remove it', function () {
       const queue = new Queue()
       queue.offer(1)
       queue.offer(2)
       queue.offer(3)
-      assert.deepEqual(3, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
-      assert.deepEqual(1, queue.poll())
-      assert.deepEqual(2, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 3)
+      assert.deepStrictEqual(queue.isEmpty, false)
+      assert.deepStrictEqual(queue.poll(), 1)
+      assert.deepStrictEqual(queue.size, 2)
+      assert.deepStrictEqual(queue.isEmpty, false)
     })
+
     it('should empty the queue when called enough times', function () {
       const queue = new Queue()
       for (let i = 0; i < 10; i++) {
         queue.offer(i)
       }
       for (let i = 0; i < 10; i++) {
-        assert.deepEqual(i, queue.poll())
+        assert.deepStrictEqual(queue.poll(), i)
       }
-      assert.deepEqual(null, queue.poll())
-      assert.deepEqual(0, queue.size)
-      assert.deepEqual(true, queue.isEmpty)
+      assert.deepStrictEqual(queue.poll(), undefined)
+      assert.deepStrictEqual(queue.size, 0)
+      assert.deepStrictEqual(queue.isEmpty, true)
     })
   })
+
   describe('.peek()', function () {
-    it('should return null if called on an empty queue', function () {
+    it('should return undefined if called on an empty queue', function () {
       const queue = new Queue()
-      assert.deepEqual(null, queue.peek())
+      assert.deepStrictEqual(queue.peek(), undefined)
     })
+
     it('should return the right element, but not remove it', function () {
       const queue = new Queue()
       queue.offer(1)
       queue.offer(2)
       queue.offer(3)
-      assert.deepEqual(3, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
-      assert.deepEqual(1, queue.peek())
-      assert.deepEqual(3, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 3)
+      assert.deepStrictEqual(queue.isEmpty, false)
+      assert.deepStrictEqual(queue.peek(), 1)
+      assert.deepStrictEqual(queue.size, 3)
+      assert.deepStrictEqual(queue.isEmpty, false)
     })
   })
+
   describe('.clear()', function () {
     it('should clear the queue', function () {
       const queue = new Queue()
       for (let i = 0; i < 10; i++) {
         queue.offer(i)
       }
-      assert.deepEqual(10, queue.size)
-      assert.deepEqual(false, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 10)
+      assert.deepStrictEqual(queue.isEmpty, false)
       queue.clear()
-      assert.deepEqual(0, queue.size)
-      assert.deepEqual(true, queue.isEmpty)
+      assert.deepStrictEqual(queue.size, 0)
+      assert.deepStrictEqual(queue.isEmpty, true)
+      assert.deepStrictEqual(queue.peek(), undefined)
     })
   })
 })
-
-function array(bound) {
-  return [...Array(bound).keys()]
-}
