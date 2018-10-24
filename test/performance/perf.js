d887a23410f6f0d3c4c9c289b0bfea615d0e599f
Emil Englesson <englesson.emil@gmail.com>
2018-10-24 16:49:26 +0200

    The benchmarks are now compared to a baseline, which does nothing at all except to execute the incoming thunk
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- test/performance/perf.js
+++ test/performance/perf.js
@@ -1,94 +1,97 @@
 'use strict'
 
-const Benchmark = require('benchmark');
+const Benchmark = require('benchmark')
 const Semaphore = require('../../index.js')
+const printPlatform = require('./print-platform.js')
+const printBenchmarks = require('./print-benchmarks.js')
 
-const suite = new Benchmark.Suite
+runAllBenchmarks()
 
-const platform = Benchmark.platform
-console.log('Executing tests on:')
-console.log('==============')
-console.log('Name:         ' + platform.name)
-console.log('Version:      ' + platform.version)
-console.log('Product:      ' + platform.product)
-console.log('Manufacturer: ' + platform.manufacturer)
-console.log('Layout:       ' + platform.layout)
-console.log('OS:           ' + platform.os)
-console.log('Description:  ' + platform.description)
-console.log('==============')
-
-for (let semLimit = 10; semLimit <= 10; semLimit++) {
-  for (let amount = 10; amount <= 1000000; amount = amount * 10) {
-    suite.add(generateTestCase(semLimit, amount))
+async function runAllBenchmarks() {
+  printPlatform()
+  const bounds = [10, 100, 1000, 10000, 100000, 1000000, 5000000, 10000000]
+  const benchmarks = []
+  for (const bound of bounds) {
+    const result = await runBasicLockTest(bound)
+    benchmarks.push(result)
   }
+  printBenchmarks(benchmarks)
 }
 
-let totalTests = suite.length
-let finished = 0
-suite
-  .on('cycle', function (event) {
-    finished += 1
-    console.log(finished + '/' + totalTests + ' completed: ' + event.target);
+async function runBasicLockTest(nbrOfPromises) {
+  const info = 'Execute ' + nbrOfPromises + ' promises'
+  return runSuite(info, () => {
+    const promises = array(nbrOfPromises).map(() => promiseFunc())
+    return createBasicPerfSuite(sem => {
+      return deferred => {
+        const allPromises = promises.map(promise => sem.lock(promise))
+        Promise.all(allPromises)
+          .then(() => deferred.resolve())
+      }
+    })
   })
-  .on('complete', function () {
-    console.log('============>')
-    for (var i = 0; i < this.length; i++) {
-      const benchmark = this[i]
-      console.log(benchmark.toString());
-      console.log('    ' + benchmark.times.period.toFixed(2) + ' s per test')
+}
+
+function runSuite(info, suiteFactory) {
+  return new Promise((resolve, reject) => {
+    console.log()
+    console.log(info)
+    console.log('-'.repeat(info.length))
+    const obj = {
+      title: info,
+      results: []
+    }
+    suiteFactory()
+      .on('cycle', function (e) {
+        console.log(String(e.target))
+      })
+      .on('complete', function () {
+        for (let i = 0; i < this.length; i++) {
+          obj.results.push(this[i])
+        }
+        resolve(obj)
+      })
+      .on('error', function () {
+        reject()
+      })
+      .run({ async: true })
+  })
+}
+
+function createBasicPerfSuite(createTestFunc) {
+  let suite = new Benchmark.Suite()
+  const semaphores = getSemaphores()
+
+  semaphores.forEach(obj => {
+    suite = suite.add(obj.name, createTestFunc(obj.semaphore), { defer: true })
+  })
+  return suite
+}
+
+function getSemaphores() {
+  const queues = []
+
+  queues.push({ name: 'Semaphore(1)', semaphore: new Semaphore(1) })
+  queues.push({ name: 'Semaphore(2)', semaphore: new Semaphore(2) })
+  queues.push({ name: 'Semaphore(10)', semaphore: new Semaphore(10) })
+  queues.push({ name: 'Semaphore(100)', semaphore: new Semaphore(100) })
+  queues.push({ name: 'Semaphore(1000)', semaphore: new Semaphore(1000) })
+  queues.push({ name: 'Semaphore(10000)', semaphore: new Semaphore(10000) })
+  queues.push({
+    name: 'baseline', semaphore: {
+      lock: function (thunk) {
+        return thunk()
+      }
     }
   })
-  .run({ 'async': true });
 
-function generateTestCase(semLimit, taskCount) {
-  function array(bound) {
-    return [...Array(bound).keys()]
-  }
-  function setup() {
-    // Why globals? See end of file
-    global.sem = new Semaphore(semLimit)
-    global.promises = array(taskCount).map(() => () => new Promise((resolve, reject) => process.nextTick(resolve)))
-  }
-  function fn(deferred) {
-    const allPromises = promises.map((promise) => sem.lock(promise))
-    Promise.all(allPromises)
-      .then(() => deferred.resolve())
-  }
-  return {
-    setup: setup,
-    fn: fn,
-    defer: true,
-    name: 'Semaphore(' + semLimit + ') -> ' + taskCount + ' promises'
-  }
+  return queues
 }
 
-// SOURCE: https://github.com/bestiejs/benchmark.js/issues/51
+function array(bound) {
+  return [...Array(bound).keys()]
+}
 
-// This is because when you pass a function to benchmark.js it will attempt to decompile it and reconstruct it in a test loop.
-//
-// .add('create graph', function() {
-//     schedule.dependencyGraph(tasks);
-// },
-// {
-//     'setup': function() {
-//         var tasks = ['A', 'B', 'C', 'D', 'E'];
-//     }
-// })
-// becomes:
-//
-// var tasks = ['A', 'B', 'C', 'D', 'E'];
-//
-// while (....) {
-//   schedule.dependencyGraph(tasks);
-// }
-// This would be great, however functions compiled in Node don't have access to variables your modules scope. So it can't access schedule and so throws and error and attempts to fallback to the non-compiled approach which then turns your code into:
-//
-// this.setup();
-// while (....) {
-//   schedule.dependencyGraph(tasks);
-// }
-// and now the test doesn't have access to tasks and throws an error.
-//
-// To avoid this define schedule on the global object like:
-//
-// global.schedule = require('../index');
+function promiseFunc() {
+  return () => new Promise((resolve, reject) => process.nextTick(resolve))
+}
